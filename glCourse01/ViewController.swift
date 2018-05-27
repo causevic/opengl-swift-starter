264a58318270c3c2aa845c46ade2f1bf85c7e9ac
Emir Causevic <causevic@gmail.com>
2018-05-27 04:28:29 +0200

    Initial Commit

---
+++ glCourse01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  glCourse01
+//
+//  Created by Emir Causevic on 5/27/18.
+//  Copyright © 2018 ecdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
